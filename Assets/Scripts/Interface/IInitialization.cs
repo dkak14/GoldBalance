cc0553a94215f9363612e7c1c7f23bfea565bd87
dkak14 <dkak_14@naver.com>
2021-12-07 16:30:42 +0900

    Revert "테스트"
    This reverts commit 101a7502e11665482742f195cb4ed6114f75b511.

--- Assets/Scripts/Interface/IInitialization.cs
+++
@@ -1,3 +0,0 @@
-public interface IInitialization {
-    public void Initialization();
-}
--- Assets/Scripts/Screen/ScreenEffect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ScreenEffect : MonoBehaviour
-{
-    public abstract void FadeIn(float duration, float screenValue);
-    public abstract void FadeOut(float duration, float screenValue);
-}
